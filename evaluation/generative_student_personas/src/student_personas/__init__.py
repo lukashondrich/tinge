48ee4b42ffb6be2858b4e92ffde054d0e0127204
lukashondrich <37348716+lukashondrich@users.noreply.github.com>
2025-09-11 17:17:59 +0200

    feat: restore likert sampling

--- evaluation/generative_student_personas/src/student_personas/__init__.py
+++ evaluation/generative_student_personas/src/student_personas/__init__.py
@@ -1,10 +1,12 @@
 from .persona import Persona
 from .dimensions import DIMENSIONS, KEYS
 from .cli import main as cli_main
+from .sampling import sample_likert_pool
 
 __all__ = [
     "Persona",
     "DIMENSIONS",
     "KEYS",
     "cli_main",
+    "sample_likert_pool",
 ]
